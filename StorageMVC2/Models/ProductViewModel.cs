dc61c611f2a12c102139afa389a6ba46bdcd3ed0
Sara Binazir <sarabinazir@gmail.com>
2019-12-05 16:37:49 +0100

    Add project files.

---
+++ StorageMVC2/Models/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageMVC2.Models
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Product> product { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public int Count { get; set; }
+
+        public int InventoryValue { get; set; }
+    }
+}
